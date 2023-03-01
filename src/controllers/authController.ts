1933a9a719a66e3dab907d0f18d3421ccdf4971a
Hoon333 <wkdzhd2000@gmail.com>
2023-03-01 16:52:25 +0900

    FIX: router.ts ADD: axios in all html files

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,2 +1,5 @@
 import jwt from "jsonwebtoken";
 
+export {
+
+}
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,3 +1,5 @@
 import express, { Router } from "express";
 const router: Router = express.Router()
 
+import * as AuthController from "../controllers/authController"
+
