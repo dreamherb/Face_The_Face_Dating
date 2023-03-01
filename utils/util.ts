1bc213c411686c11e42bd412ef106cf174508625
Hoon333 <wkdzhd2000@gmail.com>
2023-03-01 16:24:19 +0900

    ADD: util.ts add, asyncWrapper created

---
+++ utils/util.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+
+// asyncWrapper, 비동기 처리와 동시에 에러 핸들링을 용이하게 해주기 위함
+export const asyncWrapper = (asyncFn: (arg0: Request, arg1: Response, arg2: NextFunction) => any) => {
+      return (async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          return await asyncFn(req, res, next);
+        } catch(error) {
+          console.error(error);
+          return res.status(500).json({
+            isSuccess: false,
+            msg: "Internal Server Error",
+          });
+        };
+      });
+    }
