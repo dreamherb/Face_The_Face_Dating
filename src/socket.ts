af422412d9a502ecd7cc5fac4ff199b7245d057c
Hoon333 <wkdzhd2000@gmail.com>
2023-03-12 17:32:47 +0900

    ADD: chat room and server socket connection applied

--- src/socket.ts
+++ src/socket.ts
@@ -3,16 +3,41 @@ import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 const server = createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+// 활용가능 인터페이스 설정 예시
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
+
+const io = new Server<ClientToServerEvents, InterServerEvents, SocketData>(
+  server,
+  {
+    cors: {
+      origin: "*",
+    },
+  }
+);
 
 io.on("connection", (socket: Socket) => {
-  console.log("socket.io 실행");
+  console.log("서버측 socket.io 실행", socket.id);
 
-  
+  socket.on("disconnecting", (reason) => {
+    console.log("disconnecting이 먼저 실행되고 있습니다.");
+    console.log("Reason은 이것입니다.", reason);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log("disconnect가 나중에 실행되었습니다.");
+    console.log("Reason은 이것입니다.", reason);
+  });
 });
 
 export { server };
