3e8ba03705740af34399c0d548a1de1debfdc427
Hoon333 <wkdzhd2000@gmail.com>
2023-03-14 19:38:27 +0900

    ADD: profile edit function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -92,13 +92,21 @@ const get = {
     }
   ),
 
-  nickname: asyncWrapper(
+  userInfo: asyncWrapper(
     async (req: Request, res: Response, next: NextFunction) => {
       const { user } = res.locals;
-      const { nickname } = user;
+      const targetUser = await userRepository.findOne({
+        where: {
+          user_no: user.user_no,
+        },
+        relations: {
+          profile: true,
+        },
+      });
+
       return res.status(200).json({
         isSuccess: true,
-        nickname,
+        user: targetUser,
       });
     }
   ),
@@ -123,13 +131,13 @@ const get = {
         console.log("userlist가 조회되지 않았습니다!");
       }
 
-      // const userListWithoutMe = userList.filter(
-      //   (v) => v.user_no !== user.user_no
-      // );
+      const userListWithoutMe = userList.filter(
+        (v) => v.user_no !== user.user_no
+      );
 
       return res.status(200).json({
         isSuccess: true,
-        userList: userList,
+        userList: userListWithoutMe,
       });
     }
   ),
@@ -137,7 +145,41 @@ const get = {
 
 const update = {
   profile: asyncWrapper(
-    async (req: Request, res: Response, next: NextFunction) => {}
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { user } = res.locals;
+      const { gender, region, birthYear, statusMsg } = req.body;
+
+      const yearRegExp: RegExp = /([12][0-9][0-9][0-9])/g;
+      if (!yearRegExp.test(birthYear) || !(birthYear.length === 4)) {
+        return res.json({
+          isSuccess: false,
+          msg: "출생연도를 제대로 입력해주세요.",
+        });
+      }
+
+      const targetUser = await userRepository.findOne({
+        where: {
+          user_no: user.user_no,
+        },
+        relations: {
+          profile: true,
+        },
+      });
+
+      targetUser!.profile.gender = gender;
+      targetUser!.profile.birth_year = birthYear;
+      targetUser!.profile.region = region;
+      targetUser!.profile.status_msg = statusMsg;
+
+      console.log("수정할떄 targetUser는 이거다!", targetUser);
+
+      await userRepository.save(targetUser!); // entity에서 cascade 옵션을 주었기에 user만 저장해도 profile까지 같이 저장된다.
+
+      return res.json({
+        isSuccess: true,
+        msg: "프로필 수정에 성공하였습니다!",
+      });
+    }
   ),
 
   pageRefreshedTime: asyncWrapper(
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -12,7 +12,7 @@ router.patch("/auth/logout", middleware.auth.tokenConfirmation, AuthController.l
 
 // /api/user
 router.get("/user/profile",middleware.auth.tokenConfirmation ,UserController.get.profile); // 유저 프로필 존재여부 검사
-router.get("/user/info", middleware.auth.tokenConfirmation, UserController.get.nickname); // 유저 닉네임 조회
+router.get("/user/info", middleware.auth.tokenConfirmation, UserController.get.userInfo); // 유저 정보 조회
 router.post("/user/new_profile",middleware.auth.tokenConfirmation, UserController.create.profile ) // 프로필 생성
 router.patch("/user/profile_update",middleware.auth.tokenConfirmation, UserController.update.profile ) // 프로필 수정
 router.patch("/user/refreshed_timeLog", middleware.auth.tokenConfirmation, UserController.update.pageRefreshedTime) // 채팅리스트 새로고침 시간 업데이트
